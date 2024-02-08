0fd330833742f5669f3127518839cfa20766ecbd
David <www.wealthydavid@gmail.com>
2024-02-08 06:38:13 +0100

    commit in FEB'2024

---
+++ SquareOfAnyCharacter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SquareOfAnyCharacter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the side length of the square: ");
+        int side = input.nextInt();
+        System.out.print("Enter the character to fill the square: ");
+        char fillCharacter = input.next().charAt(0);
+        squareOfAnyCharacter(side, fillCharacter);
+    }
+
+    public static void squareOfAnyCharacter(int side, char fillCharacter) {
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                System.out.print(fillCharacter);
+            }
+            System.out.println();
+        }
+    }
+}
