d4850d1323bae4a3e3bbb4badf26d28359cc4d0f
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-23 13:34:52 +0100

    LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -6,4 +6,13 @@ public class LeapYear {
         System.out.print("Enter a year: ");
         int year = input.nextInt();
         if (isLeapYear(year)) {
-          
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
