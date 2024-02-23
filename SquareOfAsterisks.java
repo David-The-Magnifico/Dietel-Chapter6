d16e87447ab4901e26f446d016d1585804d9763f
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-23 13:53:30 +0100

    SquareOfAsterisks.java

--- SquareOfAsterisks.java
+++ SquareOfAsterisks.java
@@ -3,12 +3,4 @@ public class SquareOfAsterisks {
         squareOfAsterisks(4);
     }
 
-    public static void squareOfAsterisks(int side) {
-        for (int i = 0; i < side; i++) {
-            for (int j = 0; j < side; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
+   
