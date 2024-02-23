be2bf4f9eb5319fb38a8b2d5d1edd1cf9e94ebeb
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-23 13:39:38 +0100

    PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -12,3 +12,14 @@ public class PalindromeCheck {
         }
     }
 
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+}
