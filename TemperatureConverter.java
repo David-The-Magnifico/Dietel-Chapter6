d939431f470e42623fdf67ef3cf035d10b2d181c
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-23 13:54:58 +0100

    TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,7 +4,20 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a temperature: ");
-     
+        double temperature = input.nextDouble();
+        System.out.print("Enter the unit (C for Celsius, K for Kelvin): ");
+        char unit = input.next().charAt(0);
+
+        if (unit == 'C') {
+            System.out.println("Kelvin equivalent: " + celsiusToKelvin(temperature));
+        } else if (unit == 'K') {
+            System.out.println("Celsius equivalent: " + kelvinToCelsius(temperature));
+        } else {
+            System.out.println("Invalid unit.");
+        }
+    }
+
+    public static double celsiusToKelvin(double celsius) {
         return celsius + 273.15;
     }
 
