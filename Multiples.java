0d37de158b1104dd99bd9555bb1102efadf2cb45
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-23 13:37:41 +0100

    Multiples.java

--- Multiples.java
+++ Multiples.java
@@ -7,3 +7,22 @@ public class Multiples {
         while (true) {
             System.out.print("Enter the first integer (or type 'quit' to exit): ");
             String input1 = scanner.next();
+
+            if (input1.equals("quit")) {
+                System.out.println("Exiting the program...");
+                break;
+            }
+
+            System.out.print("Enter the second integer: ");
+            int num1 = Integer.parseInt(input1);
+            int num2 = scanner.nextInt();
+
+            boolean isMultiple = isMultiple(num1, num2);
+            System.out.println(num2 + " is a multiple of " + num1 + ": " + isMultiple);
+        }
+    }
+
+    public static boolean isMultiple(int first, int second) {
+        return second % first == 0;
+    }
+}
